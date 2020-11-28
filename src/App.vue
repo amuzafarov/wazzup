998160398a945c8ed507bdd15460bce703c057ac
Muzafarzoda Asliddin <bum9696@gmail.com>
2020-11-28 10:39:02 +0500

    create load users button

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
+    <Button />
   </div>
 </template>
 
 <script>
-
+import Button from './components/Button';
 export default {
   name: 'App',
   components: {
-    
+    Button
   }
 }
 </script>
 
 <style>
-
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button id="load-users-btn">Load Users</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#load-users-btn {
+    width: 200px;
+    height: 60px;
+    background: rgb(118, 13, 255);
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    outline: 0;
+    font-weight: bold;
+}
+#load-users-btn:hover {
+    border: none;
+    background: rgba(118, 13, 255, 0.787);
+}
+#load-users-btn:focus {
+    background: rgb(61, 2, 138);
+}
+</style>
